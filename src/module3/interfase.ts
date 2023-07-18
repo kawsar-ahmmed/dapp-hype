6e14cc358e46afe90cb611987d1ca4c706459651
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-07-19 00:19:35 +0600

    Advance part

--- src/module3/interfase.ts
+++ src/module3/interfase.ts
@@ -6,25 +6,31 @@ type User = {
 interface IUser {
     name: string,
     age: number
-}
+};
 
 interface IExtendedUser extends IUser {
     role: string,
-}
+};
 
-const userWithTypeAlias : User = {
+const userWithTypeAlias: User = {
     name: 'Jorina',
     age: 18
 };
 
-
-const userWithExtendInterfaceAlias : IExtendedUser = {
+const userWithExtendInterfaceAlias: IExtendedUser = {
     name: 'Sokina',
     age: 19,
     role: 'Admin'
-}
-const userWithInterfaceAlias : IUser = {
+};
+const userWithInterfaceAlias: IUser = {
     name: 'Kamina',
     age: 19,
+};
 
-}
+type addNumberType = (num1: number, num2: number) => number;
+type addNumberTypeStyle = {
+    num1: number,
+    num2: number
+};
+
+const addNumber: addNumberType = (num1, num2): number => num1 + num2;
